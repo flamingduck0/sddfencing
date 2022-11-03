b5ed8b6bf58ce147eaf43f2e04107ec473bae2b2
flamingduck0 <440984240@student.sbhs.nsw.edu.au>
2022-11-03 12:30:54 +1100

    test heroku

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    output = render_template('base.html')
+    return output
 
 
 if __name__ == '__main__':
